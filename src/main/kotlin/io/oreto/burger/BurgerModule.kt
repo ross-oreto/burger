daedab15467b5f404f79005c441bf9da3e7a1151
ross-oreto <ross.oreto@gmail.com>
2019-08-19 15:43:28 -0500

    refactoring to provide easy passing of server data to javascript variables.

--- src/main/kotlin/io/oreto/burger/BurgerModule.kt
+++ src/main/kotlin/io/oreto/burger/BurgerModule.kt
@@ -3,33 +3,49 @@ package io.oreto.burger
 import io.jooby.Kooby
 
 class BurgerModule : Kooby({
-    val defaultPages = Burger.list.size + 1
 
-    val yearParamName = "year"
-    val pageParamName = "page"
-
-    path("/burgers/{$yearParamName}") {
+    path("/${group.name}/{${group.year}}") {
         get("/") {
             views.burger.template(null, null
-                    , Server(ctx).pathParam(yearParamName, ctx.path(yearParamName).intValue())
-                    .pathParam(pageParamName, defaultPages)
-                    .param("pages", defaultPages)
+                    , Server(ctx).pathParam(group.year.name, ctx.path(group.year.name).intValue())
+                    .pathParam(group.page.name, group.page.defaultValue)
+                    .param("pages", group.page.defaultValue)
                     .param("pageId", "landing")
                     .withJs())
         }
 
-        get("/{$pageParamName}") {
-            val page: Int = ctx.path(pageParamName).intValue()
+        get("/{${group.page.name}}") {
+            val page: Int = ctx.path(group.page.name).intValue()
 
             val burger: Burger? = Burger.at(page)
             val pageId: String = burger?.id ?: (if (page == 0) "credits" else "landing")
 
             views.burger.template(burger, Taster.list
-                    , Server(ctx).pathParam(yearParamName, ctx.path(yearParamName).intValue())
-                    .pathParam(pageParamName, page)
-                    .param("pages", defaultPages)
+                    , Server(ctx).pathParam(group.year.name, ctx.path(group.year.name).intValue())
+                    .pathParam(group.page.name, page)
+                    .param("pages", group.page.defaultValue)
                     .param("pageId", pageId)
                     .withJs())
         }
     }
-})
+}) {
+    companion object {
+        interface Grouped { val name: String }
+        interface Named {
+            val name: String
+            val defaultValue: Any?
+        }
+        val group = BurgerGroup("burgers")
+
+        class BurgerGroup(override val name: String): Grouped {
+            inner class Year(override val name: String = "year"
+                             , override val defaultValue: Any? = 2019): Named
+
+            inner class Page(override val name: String = "page"
+                             , override val defaultValue: Any? = Burger.list.size + 1): Named
+
+            val page = Page()
+            val year = Year()
+        }
+    }
+}
